85fa69d3c86232f317669e3a0518b479033a6b3d
Bruce Schubert <bruce@emxsys.com>
2016-07-29 10:08:26 -0700

    Configured AndroidGetStarted child router to support bookmarks.

--- js/viewModels/AndroidQuickStart.js
+++ js/viewModels/AndroidQuickStart.js
@@ -9,43 +9,64 @@
 define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojrouter', 'ojs/ojnavigationlist'
 ], function (oj, ko, $) {
 
-    // Some starter/seed code to implement a child router so the pages can be bookmarked and for back/prev navigation
-    //var parentRouter = oj.Router.rootInstance.getChildRouter('androidtopic'),
-    //    childRouter = parentRouter.createChildRouter('getstarted', 'androidtopic');
-    //
-    //childRouter.configure({
-    //    'overview': {label: 'Overview', value: "AndroidQuickStartOverview", isDefault: true},
-    //    'setup': {label: 'Setup', value: "AndroidQuickStartSetup"},
-    //    'build': {label: 'Build', value: 'AndroidQuickStartBuild'},
-    //    'run': {label: 'Run', value: 'AndroidQuickStartRun'},
-    //    'tutorials': {label: 'Tutorials', value: "AndroidExamples"}
-    //});
-
+    // Use caution when changing router names and state ids, as a change could break
+    // a user's bookmark to that page.
+    
+    // Create a child router named 'getstartedtopic' for the androidtopic's 'getstarted' state
+    var androidRouter = oj.Router.rootInstance.getChildRouter('androidtopic'),
+        childRouter = androidRouter.createChildRouter('getstartedtopic', 'getstarted');
+    
+    childRouter.configure({
+        'overview': {label: 'Overview', value: "AndroidQuickStartOverview", isDefault: true},
+        'install': {label: 'Setup', value: "AndroidQuickStartInstall"},
+        'build': {label: 'Build', value: 'AndroidQuickStartBuild'},
+        'run': {label: 'Run', value: 'AndroidQuickStartRun'},
+        'basicglobe': {label: 'Tutorials', value: "AndroidTutorialBasicGlobe"},
+        'wmslayer': {label: 'Tutorials', value: "AndroidTutorialWmsLayer"},
+        'surfaceimage': {label: 'Tutorials', value: "AndroidTutorialSurfaceImage"},
+        'lookatview': {label: 'Tutorials', value: "AndroidTutorialLookAtView"},
+        'cameraview': {label: 'Tutorials', value: "AndroidTutorialCameraView"},
+        'navigatorevents': {label: 'Tutorials', value: "AndroidTutorialNavigatorEvents"},
+        'placemarks': {label: 'Tutorials', value: "AndroidTutorialPlacemarks"},
+        'placemarkpicking': {label: 'Tutorials', value: "AndroidTutorialPlacemarkPicking"},
+        'cameracontrols': {label: 'Tutorials', value: "AndroidTutorialCameraControls"},
+        'showtessellation': {label: 'Tutorials', value: "AndroidTutorialShowTessellation"}
+    });
+    oj.Router.sync();
+    
     /**
      * The view model for the Android SDK content view template
      */
     function AndroidQuickStart() {
         var self = this;
-
-        this.itemOnly = function (context) {
+        
+        this.router = childRouter;
+        
+        // In the ojNavigationList, we don't allow headings to be selected, only leaf items.
+        this.isLeaf = function (context) {
             return context['leaf'];
         };
+        
+        // The selected item in the view's ojNavigationList
         this.selectedItem = ko.observable(AndroidQuickStart.lastSelectedItem);
+        
+        // When the selected item changes, syncronize the router 
+        // and as a result the page content's to the current selection.
         this.selectedItem.subscribe(function (newItem) {
-            AndroidQuickStart.lastSelectedItem = newItem;
-            // Ensure the parent list item is expanded
-            // Get the parent <ul> and then the parent <li>
+            self.router.go(newItem);
+            
+            // Ensure the parent item is expanded in the navigation list. 
+            // This is req'd when the selection is made programmatically.
+            // Use JQuery to get the parent <ul> and then the parent <li>
+            // TODO: This code should be in a routerState event handler.
             var $parent = $("#" + newItem).parent().parent();
             if ($parent.length) {
                 $("#android-getstarted-menu").ojNavigationList("expand", $parent.attr("id"));
             }
+            
+            // Remember this selection so we can restore it if the view is recreated
+            AndroidQuickStart.lastSelectedItem = newItem;
         });
-
-
-        this.showModule = function (module) {
-            self.selectedItem(module);
-            //childRouter.go('/android/getstarted/run')
-        }
     }
 
     AndroidQuickStart.lastSelectedItem = "AndroidQuickStartOverview";
