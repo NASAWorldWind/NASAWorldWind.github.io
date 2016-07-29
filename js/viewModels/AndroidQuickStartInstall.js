cdcc3363a5bcabb4aa7a703855a645d523dcda4e
Bruce Schubert <bruce@emxsys.com>
2016-07-29 10:10:20 -0700

    Removed boiler plate code.

--- js/viewModels/AndroidQuickStartInstall.js
+++ js/viewModels/AndroidQuickStartInstall.js
@@ -4,21 +4,17 @@
  */
 
 /**
- * AndroidQuickStartSetup module
+ * AndroidQuickStartInstall module
  */
 define(['ojs/ojcore', 'knockout'
 ], function (oj, ko) {
     /**
      * The view model for the main content view template
      */
-    function AndroidQuickStartSetupContentViewModel() {
+    function AndroidQuickStartInstall() {
         var self = this;
-        self.firstName = ko.observable("Planet");
-        self.lastName = ko.observable("Earth");
-        self.fullName = ko.pureComputed(function () {
-            return this.firstName() + " " + this.lastName();
-        }, this);
+
     }
     
-    return AndroidQuickStartSetupContentViewModel;
+    return AndroidQuickStartInstall;
 });
