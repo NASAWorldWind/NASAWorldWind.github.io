8397c184ebb32647451f3c192c94ea289a22cb6a
Bruce Schubert <bruce@emxsys.com>
2016-11-03 07:22:04 -0700

    Added Android Links page

---
+++ js/viewModels/AndroidLinks.js
@@ -0,0 +1,20 @@
+/* 
+ * Copyright (C) 2016 United States Government as represented by the Administrator of the
+ * National Aeronautics and Space Administration. All Rights Reserved.
+ */
+
+/**
+ * AndroidExamples module
+ */
+define(['ojs/ojcore', 'knockout', 'ojs/ojrouter', 'ojs/ojnavigationlist'
+], function (oj, ko) {
+    /**
+     * The view model for the Android Links content
+     */
+    function AndroidLinks() {
+        var self = this;
+
+    };
+
+    return AndroidLinks;
+});
