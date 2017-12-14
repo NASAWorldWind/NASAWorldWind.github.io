96a602803f634745ea41bbb98d30b6f7632e9c58
eirizarry <endiajanae@mail.fresnostate.edu>
2017-12-14 11:59:07 -0800

    Track only outbound links
    - recursively find href for Web and Android examples
    - exclude tracking internal clicks

--- static/js/event-tracking.js
+++ static/js/event-tracking.js
@@ -1,14 +1,34 @@
-// Get elements by tag name and tracks onclick events
+// Get elements by tag name and track outbound links
 
 var elements = document.getElementsByTagName('a');
 for(var i = 0, len = elements.length; i < len; i++) {
-    elements[i].onclick = function handleOutboundLinkClicks(event) {
 
-        ga('send', 'event', {
-            eventCategory: 'Outbound Link',
-            eventAction: 'click',
-            eventLabel: event.target.href,
-            transport: 'beacon'
-        });
+    var internal = (elements[i].host === 'worldwind.arc.nasa.gov'));
+
+    if (!internal) {
+        elements[i].onclick = function handleOutboundLinkClicks(event) {
+
+            var findHref = function (element) {
+                if (element.href) {
+                    return element.href;
+                } else {
+                    if (element.parentElement) {
+                        return findHref(element.parentElement);
+                    } else {
+                        return null;
+                    }
+                }
+
+            };
+
+            var targetHref = findHref(event.target);
+
+            ga('send', 'event', {
+                eventCategory: 'Outbound Link',
+                eventAction: 'click',
+                eventLabel: targetHref,
+                transport: 'beacon'
+            });
+        }
     }
 }
